59ef36a0bf4cc22ef9499f714b9591e808288ff5
JoelBiere <76972193+JoelBiere@users.noreply.github.com>
2024-04-07 19:23:03 -0500

    starting to connect firebase

---
+++ src/firebase/register-to-firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAnLlzwz51wtxXBs1VifL6HPdFHSVaZMRI",
+    authDomain: "wordle-your-friends.firebaseapp.com",
+    databaseURL: "https://wordle-your-friends-default-rtdb.firebaseio.com",
+    projectId: "wordle-your-friends",
+    storageBucket: "wordle-your-friends.appspot.com",
+    messagingSenderId: "925748911872",
+    appId: "1:925748911872:web:ebaa59135506e5dd5b5de5",
+    measurementId: "G-4L20R015VQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
